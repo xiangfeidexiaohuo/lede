73bd3363b5917a12aecb3041c56f39f0a57e8cee
xiangfeidexiaohuo <jsjson@163.com>
2023-02-03 22:38:35 +0800

    Complete and optimize some files for ipq807x of XiaoMi

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -45,6 +45,41 @@ define Device/zte_mf269
 endef
 TARGET_DEVICES += zte_mf269
 
+define Device/redmi_ax6
+	$(call Device/xiaomi_ax3600)
+	DEVICE_VENDOR := Redmi
+	DEVICE_MODEL := AX6
+	DEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools
+endef
+TARGET_DEVICES += redmi_ax6
+
+define Device/xiaomi_ax3600
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Xiaomi
+	DEVICE_MODEL := AX3600
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@ac04
+	SOC := ipq8071
+	DEVICE_PACKAGES := ath10k-firmware-qca9887-ct ipq-wifi-xiaomi_ax3600 \
+	kmod-ath10k-ct uboot-envtools
+endef
+TARGET_DEVICES += xiaomi_ax3600
+
+define Device/xiaomi_ax9000
+	$(call Device/FitImage)
+	$(call Device/UbiFit)
+	DEVICE_VENDOR := Xiaomi
+	DEVICE_MODEL := AX9000
+	BLOCKSIZE := 128k
+	PAGESIZE := 2048
+	DEVICE_DTS_CONFIG := config@hk14
+	SOC := ipq8072
+	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax9000 kmod-ath10k-ct ath10k-firmware-qca9887-ct
+endef
+TARGET_DEVICES += xiaomi_ax9000
+
 define Device/tplink_tl-er2260t
 	$(call Device/FitImage)
 	$(call Device/UbiFit)  
